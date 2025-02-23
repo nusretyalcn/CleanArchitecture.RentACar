d580c5d9850f9e77b722677b908e76b933d8049f
nusretyalcn <yalcnusret@gmail.com>
2025-02-23 19:54:03 +0300

    Automapper implementation

--- src/CleanArchitecture.RentAcar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ src/CleanArchitecture.RentAcar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,4 +1,5 @@
 using Application.Services.Repositories;
+using AutoMapper;
 using Domain.Entities;
 using MediatR;
 
@@ -11,22 +12,21 @@ public class CreateBrandCommand:IRequest<CreatedBrandResponse>
     public class CreateBrandCommandHandler: IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
     {
         private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
 
-        public CreateBrandCommandHandler(IBrandRepository brandRepository)
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
         {
             _brandRepository = brandRepository;
+            _mapper = mapper;
         }
 
         public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
-            Brand brand = new Brand();
-            brand.Name = request.Name;
+            Brand brand = _mapper.Map<Brand>(request);
             brand.Id = Guid.NewGuid();
             
             var result = await _brandRepository.AddAsync(brand);
-            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
-            createdBrandResponse.Id = result.Id;
-            createdBrandResponse.Name = result.Name;
+            CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(result);
             
             return createdBrandResponse;
         }
