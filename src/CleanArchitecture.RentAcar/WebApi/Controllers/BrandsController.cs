b4aac8979b7c1018615338a4eca1d935c3b2ee58
nusretyalcn <yalcnusret@gmail.com>
2025-02-25 22:55:12 +0300

    Brand Queries added

--- src/CleanArchitecture.RentAcar/WebApi/Controllers/BrandsController.cs
+++ src/CleanArchitecture.RentAcar/WebApi/Controllers/BrandsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +19,14 @@ namespace WebApi.Controllers
         public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
         {
             CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new () { PageRequest = pageRequest };
+            GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(getListBrandQuery);
             return Ok(response);
         }
     }
